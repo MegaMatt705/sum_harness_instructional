46d89126e90c610b335af3cd6ff876e98b2e1829
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-04 15:54:26 -0700

    3 variables

--- plot_3vars_savefig.py
+++ plot_3vars_savefig.py
@@ -11,20 +11,31 @@ var_names = list(df.columns)
 
 print("var names =", var_names)
 
-# Assuming the % peak memory bandwidth utilized metric is in the second column
-memory_bandwidth = df[var_names[1]].values.tolist()
-
-plt.figure()
-
-plt.title("Problem Size vs. % Peak Memory Bandwidth Utilized")
-
 # Assuming the problem size is in the first column
 problem_sizes = df[var_names[0]].values.tolist()
 
-plt.plot(problem_sizes, memory_bandwidth, "r-o")
+# Assuming MFLOP/s metrics are in the second, third, and fourth columns
+mflops_code1 = df[var_names[1]].values.tolist()
+mflops_code2 = df[var_names[2]].values.tolist()
+mflops_code3 = df[var_names[3]].values.tolist()
+
+plt.figure()
+
+plt.title("Problem Size vs. MFLOP/s for 3 Codes")
+
+xlocs = [i for i in range(len(problem_sizes))]
+
+plt.xticks(xlocs, problem_sizes)
+
+# Plotting MFLOP/s for each code
+plt.plot(mflops_code1, "r-o", label=var_names[1])
+plt.plot(mflops_code2, "b-x", label=var_names[2])
+plt.plot(mflops_code3, "g-^", label=var_names[3])
 
 plt.xlabel("Problem Sizes")
-plt.ylabel("% Peak Memory Bandwidth Utilized")
+plt.ylabel("MFLOP/s")
+
+plt.legend(loc="best")
 
 plt.grid(axis='both')
 
